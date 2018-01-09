6772d52faf54db0f7a6518a658904eb6cb041330
Jason Alsip <13713268+jbalsip@users.noreply.github.com>
2018-01-08 22:15:46 -0500

    Add login to sidebar so it is more useful

--- bootstrap-theme/sidebar.php
+++ bootstrap-theme/sidebar.php
@@ -17,4 +17,13 @@
       <li><a href="#">Facebook</a></li>
     </ol>
   </div>
+  <div class="sidebar-module">
+    <ol class="list-unstyled">
+    <?php if (is_user_logged_in()) : ?>
+      <li><a href="<?php echo wp_logout_url(get_permalink()); ?>">Logout</a></li>
+    <?php else : ?>
+      <li><a href="<?php echo wp_login_url(get_permalink()); ?>">Login</a></li>
+    <?php endif;?>
+    </ol>
+  </div>
 </div><!-- /.blog-sidebar -->
