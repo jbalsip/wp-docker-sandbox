5eccf594519d0e5c69758847c9ae382404671145
kapipan <35180094+kapipan@users.noreply.github.com>
2018-03-04 19:51:56 -0500

    Trying pull blog contents... but something is wrong

--- kapi-theme/content.php
+++ kapi-theme/content.php
@@ -1,54 +1,11 @@
-            <div class="root-container">
-                <div class="main-contents">
-                    <div class="blog-posts-container">
                         <div class="blog-post-summary">
                             <div class="image-container">
                                 <img class="featured-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/1.jpg" alt="stationaries on a desk"></div>
-                            <p class="date-published">2018-01-05</p>
-                            <h2 class="blog-post-title">Our all new kapibara collection</h2>
-                            <p class="">Feet evil to hold long he open knew an no. Apartments occasional boisterous as solicitude to introduced. Or fifteen covered we enjoyed demesne is in prepare.</p>
+                            <p class="date-published">
+                                <?php the_date(); ?> by <a href="#"><?php the_author(); ?></a>
+                            </p>
+                            <h2 class="blog-post-title"><?php the_title(); ?></h2>
+                            <p class=""><?php the_content(); ?></p>
                         </div>
-                        <div class="blog-post-summary">
-                            <div class="image-container">
-                                <img class="featured-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/kuma.png" alt="laptop and coffee">
-                            </div>
-                            <p class="date-published">2018-01-02</p>
-                            <h2 class="blog-post-title">High quality benefits in products</h2>
-                            <p class="">A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. </p>
-                        </div>
-                        <div class="blog-post-summary">
-                            <div class="image-container">
-                                <img class="featured-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/kuma2.jpg" alt="coffee and plant">
-                            </div>
-                            <p class="date-published">2017-12-30</p>
-                            <h2 class="blog-post-title">Building your brand is simple</h2>
-                            <p class="">My resembled in forfeited sportsman Which vexed did began son abode short may. Interested astonished he at cultivated or me. </p>
-                        </div>
-                        <div class="blog-post-summary">
-                            <div class="image-container">
-                                <img class="featured-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/4.jpg" alt="pencil and eraser">
-                            </div>
-                            <p class="date-published">2017-12-15</p>
-                            <h2 class="blog-post-title">Take a break you deserve it</h2>
-                            <p class="">Lasted my coming uneasy marked so should. Gravity letters it amongst herself dearest an windows by. Wooded ladies she basket season age her uneasy saw. </p>
-                        </div>
-                        <div class="blog-post-summary">
-                            <div class="image-container">
-                                <img class="featured-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/5.jpg" alt="plant drawings">
-                            </div>
-                            <p class="date-published">2017-11-23</p>
-                            <h2 class="blog-post-title">Produxtivity tools & tips</h2>
-                            <p class="">Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.</p>
-                        </div>
-                        <div class="blog-post-summary">
-                            <div class="image-container">
-                                <img class="featured-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/6.jpg" alt="yellow small flowers">
-                            </div>
-                            <p class="date-published">2017-11-18</p>
-                            <h2 class="blog-post-title">Staying inspired by others</h2>
-                            <p class="">Unfeeling so rapturous discovery he exquisite. Reasonably so middletons or impression by terminated. Old pleasure required removing elegance him had.</p>
-                        </div>
-                    </div>
-                    <div class="page-navigator">
-                    </div>
-                </div>
+
+                    
--- kapi-theme/index.php
+++ kapi-theme/index.php
@@ -1,4 +1,19 @@
 <?php get_header(); ?>
-<?php get_template_part( 'content', get_post_format() ); ?> 
+            <div class="root-container">
+                <div class="main-contents">
+                    <div class="blog-posts-container">
+                        <?php 
+                            if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+
+                            get_template_part( 'content', get_post_format() ); 
+                            endwhile; endif; 
+                        ?> 
+<!--                        <?php get_template_part( 'content', get_post_format() ); ?> -->
+                    </div>
+                    <div class="page-navigator">
+                    </div>
+                </div>
+
 <?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
