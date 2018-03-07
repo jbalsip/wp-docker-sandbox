b4131863cee590e45fd5706bfb1c1519da493686
kapipan <35180094+kapipan@users.noreply.github.com>
2018-03-07 15:58:35 -0500

    Add link title attribute

--- kapi-theme/content.php
+++ kapi-theme/content.php
@@ -1,12 +1,12 @@
                         <div class="blog-post-summary">
                             <div class="image-container">
-                                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium', array( 'class' => 'featured-image' )); ?></a>
+                                <a href="<?php the_permalink(); ?>", title="<?php the_title(); ?>"><?php the_post_thumbnail('medium', array( 'class' => 'featured-image' )); ?></a>
                             </div>
                             <p class="date-published">
                                 <?php the_time(get_option('date_format')); ?> by <a href="#"><?php the_author(); ?></a>
                             </p>
-                            <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                            <a href="<?php the_permalink(); ?>"><p class=""><?php the_excerpt(); ?></p></a>
+                            <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>", title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                            <a href="<?php the_permalink(); ?>", title="<?php the_title(); ?>"><p class=""><?php the_excerpt(); ?></p></a>
                         </div>
 
                     
