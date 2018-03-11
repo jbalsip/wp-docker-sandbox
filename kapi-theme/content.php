9e5136be2128d725cc76824677102aa2ae78835f
kapipan <35180094+kapipan@users.noreply.github.com>
2018-03-10 19:25:55 -0500

    Add icon for publish date

--- kapi-theme/content.php
+++ kapi-theme/content.php
@@ -3,7 +3,7 @@
                                 <a href="<?php the_permalink(); ?>", title="<?php the_title(); ?>"><?php the_post_thumbnail('medium', array( 'class' => 'featured-image thumnail' )); ?></a>
                             </div>
                             <p class="date-published">
-                                <?php the_time(get_option('date_format')); ?> by <a href="#"><?php the_author(); ?></a>
+                                <i class="far fa-clock"></i>&nbsp;<?php the_time(get_option('date_format')); ?>
                             </p>
                             <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>", title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
                             <a href="<?php the_permalink(); ?>", title="<?php the_title(); ?>"><p class=""><?php the_excerpt(); ?></p></a>
