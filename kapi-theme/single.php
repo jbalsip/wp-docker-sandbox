ae3b9a8241e214efd80ed56f4b8d8a91071feead
kapipan <35180094+kapipan@users.noreply.github.com>
2018-03-15 16:27:37 -0400

    Add updated date

--- kapi-theme/single.php
+++ kapi-theme/single.php
@@ -12,7 +12,12 @@
                                 <?php the_post_thumbnail('large', array( 'class' => 'featured-image single' )); ?>
                             </div>
                             <p class="date-published">
-                                <i class="far fa-clock"></i>&nbsp;<?php the_time(get_option('date_format')); ?> by <a href="#"><?php the_author(); ?></a>
+                                <i class="far fa-clock"></i>&nbsp;<?php the_time(get_option('date_format')); ?>
+                                <?php if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) { 
+                                        echo '&nbsp;<i class="fas fa-redo-alt"></i>&nbsp;';
+                                        the_modified_date();}
+                                ?>
+                                &nbsp;by <a href="#"><?php the_author(); ?></a>
                             </p>
                             <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>", title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
                             <p class=""><?php the_content(); ?></p>
