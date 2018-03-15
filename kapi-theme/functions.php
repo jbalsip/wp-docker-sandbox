cfebaa2da0046953f154054b7d4c559ebea340bf
kapipan <35180094+kapipan@users.noreply.github.com>
2018-03-15 09:46:50 -0400

    Add style to images in posts

--- kapi-theme/functions.php
+++ kapi-theme/functions.php
@@ -1,11 +1,6 @@
 <?php
     add_theme_support( 'post-thumbnails' );
 
-    function my_excerpt_length($length) {
-        return 20;
-    }
-    add_filter('excerpt_length', 'my_excerpt_length');
-
     function archive_navi_template($template){
 	$template = '
 	<nav class="navigation %1$s" role="navigation">
@@ -15,8 +10,22 @@
     }
     add_action( 'navigation_markup_template', 'archive_navi_template' );
 
+    function my_excerpt_length($length) {
+        return 20;
+    }
+    add_filter('excerpt_length', 'my_excerpt_length');
+
     function my_excerpt_more($more) {
     return '…';
     }
     add_filter('excerpt_more', 'my_excerpt_more');
+
+    add_filter( 'the_content', 'img_p_class_content_filter' ,20);
+    function img_p_class_content_filter($content) {
+        // assuming you have created a page/post entitled 'debug'
+        $content = preg_replace("/(<p[^>]*)(\>.*)(\<img.*)(<\/p>)/im", "\$1 class='content-img-wrap'\$2\$3\$4", $content);
+
+        return $content;
+    }
+
 ?>
