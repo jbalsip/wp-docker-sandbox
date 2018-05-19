3424416186cb417613ac856cb7d907874216277d
Unknown <35180094+kapipan@users.noreply.github.com>
2018-05-18 20:54:17 -0400

    Header Menu

--- kapi-theme/functions.php
+++ kapi-theme/functions.php
@@ -28,4 +28,9 @@
         return $content;
     }
 
+    function register_my_menu() {
+        register_nav_menu('header-menu',__( 'Header Menu' ));
+    }
+    add_action( 'init', 'register_my_menu' );
+
 ?>
--- kapi-theme/header.php
+++ kapi-theme/header.php
@@ -26,12 +26,8 @@
     <div id="page">
         <div class="menu-container">
             <nav class="topnav" id="myTopnav">
+                <?php wp_nav_menu( array( 'theme_location' => 'header-menu'));?>
                 <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#blog">Blog</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#faq">FAQ</a></li>
                     <li><a class="icon" id="click"><i class="fas fa-bars"></i></a></li>
                 </ul>
             </nav>
