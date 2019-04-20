22203df2e6c3eac248716659442fa5010ce308e4
kapipan <35180094+kapipan@users.noreply.github.com>
2019-04-19 20:16:39 -0400

    まだまだ検索機能作業中だよ(;´･ω･)

--- kapi-theme/search.php
+++ kapi-theme/search.php
@@ -1,15 +1,30 @@
 <?php get_header(); ?>
-<div class="root-container">
-                <div class="main-contents">
-                    <div class="blog-posts-container">
-                        <?php 
-                            if ( have_posts() ) : while ( have_posts() ) : the_post(); 
 
-                            get_template_part( 'content', get_post_format() ); 
-//                            the_post_thumbnail();
-                            endwhile; endif; 
-                        ?> 
-                    </div>
+<?php
+ global $wp_query;
+ $total_results = $wp_query->found_posts;
+ $search_query = get_search_query();
+?>
+
+<div class="root-container">
+    <div class="main-contents">
+        <div class="blog-posts-container">
+            <h1 class="searchresult"><?php echo $search_query; ?>: Search Results<span>（<?php echo $total_results; ?>）</span></h1>
+
+
+                <?php
+                if( $total_results >0 ):
+                if(have_posts()):
+                while(have_posts()): the_post();
+                get_template_part( 'content', get_post_format() ); 
+                ?>
+                <?php endwhile; endif; else: ?>
+
+                <?php echo $search_query; ?> is not found.
+
+                <?php endif; ?>
+
+                </div>
                     <div class="page-navigator">
                         <?php
                             // Previous/next page navigation.
@@ -21,6 +36,31 @@
                     </div>
                 </div>
 
+
+</div>
+
 <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
+
+
+<?php get_header(); ?>
+            <div class="root-container">
+
+                        <?php 
+                            if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+
+                            get_template_part( 'content', get_post_format() ); 
+//                            the_post_thumbnail();
+                            endwhile; endif; 
+                        ?> 
+
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
+
+
+
+
+
