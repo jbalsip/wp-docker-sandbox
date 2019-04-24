e02ce39ea9c301693eff0d1775695767cbf844af
kapipan <35180094+kapipan@users.noreply.github.com>
2019-04-24 15:33:28 -0400

    change date section

--- kapi-theme/content.php
+++ kapi-theme/content.php
@@ -2,9 +2,18 @@
                             <div class="image-container">
                                 <a href="<?php the_permalink(); ?>", title="<?php the_title(); ?>"><?php the_post_thumbnail('medium', array( 'class' => 'featured-image thumnail' )); ?></a>
                             </div>
-                            <p class="date-published">
-                                <i class="far fa-calendar fa-fw"></i><?php the_time(get_option('date_format')); ?>
-                            </p>
+                            <?php
+                                    if(get_the_time(‘U’) !== get_the_modified_time(‘U’))
+                                    { echo 
+                                    '<p class ="date-updated">
+                                    <i class="fas fa-redo-alt fa-fw"></i>',
+                                    the_modified_date(get_option('date_format'));}
+                                    else
+                                    { echo
+                                    '<p class ="date-published">
+                                    <i class="far fa-calendar fa-fw"></i>',
+                                    the_time(get_option('date_format'));}
+                                ?>
                             <p class="category"><i class="fas fa-tag fa-fw"></i>category</p>
                             <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>", title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
                             <a href="<?php the_permalink(); ?>", title="<?php the_title(); ?>"><?php the_excerpt(); ?></p></a>
--- kapi-theme/single.php
+++ kapi-theme/single.php
@@ -11,14 +11,18 @@
                             <div class="image-container single">
                                 <?php the_post_thumbnail('large', array( 'class' => 'featured-image single' )); ?>
                             </div>
-                            <p class="date-published">
-                                <i class="far fa-calendar fa-fw"></i><?php the_time(get_option('date_format')); ?>
-                                <?php if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) { 
-                                        echo '<i class="fas fa-redo-alt fa-fw"></i>';
-                                        the_modified_date();}
+                                <?php
+                                    if(get_the_time(‘U’) !== get_the_modified_time(‘U’))
+                                    { echo 
+                                    '<p class ="date-updated">
+                                    <i class="fas fa-redo-alt fa-fw"></i>',
+                                    the_modified_date(get_option('date_format'));}
+                                    else
+                                    { echo
+                                    '<p class ="date-published">
+                                    <i class="far fa-calendar fa-fw"></i>',
+                                    the_time(get_option('date_format'));}
                                 ?>
-                                by <a href="#"><?php the_author(); ?></a>
-                            </p>
                             <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>", title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
                             <p class=""><?php the_content(); ?></p>
                         </div>
